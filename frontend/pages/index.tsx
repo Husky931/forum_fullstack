6099d297ea2c38f74926ad8624e7b41ccdef993d
Xz33M Hask <pecevgligor@protonmail.com>
2022-12-05 14:20:57 +0100

    update

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -137,7 +137,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
         `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/threads?sort=updatedAt%3Adesc&pagination[limit]=-1`
     )
     let latestPosts = await backendData.json()
-    let bla = "1d"
+    let bla = "1dd"
 
     return {
         props: {
